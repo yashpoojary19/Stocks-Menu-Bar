4ba8e4037cb8aa40ef1fd33e245bf5a82ec66734
Yash Poojary <yash@capshun.co>
2022-05-18 19:05:04 +0530

    Initial Commit

---
+++ Stocks Menu Bar/Stocks_Menu_BarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stocks_Menu_BarApp.swift
+//  Stocks Menu Bar
+//
+//  Created by Yash Poojary on 18/05/22.
+//
+
+import SwiftUI
+
+@main
+struct Stocks_Menu_BarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
